e7598f0a4b246b025031d16873b0ce7b8e8dda15
Taynara Araújo <112909056+Thay483@users.noreply.github.com>
2023-06-01 13:33:12 -0300

    Fucionando função inclusão de voo
    Função adição de voo em progresso

--- VLA/Vermelho_linhas_aereas.c
+++ VLA/Vermelho_linhas_aereas.c
@@ -1,83 +1,120 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define NUM_CHAR 11
 #define NUM_VIAGEM 50
-
-//PROTÓTIPOS
-void inclusao_voo();
-void alteracao_info_voo();
-void exlusao_voo();
-void quant_voo_origem();
-void menor_quant_escala_voo();
+#define NUM_COD 4
 
 //ESTRUTURA
-struct Voo{
+ struct Viagem {
     int codigo;
     char cidade_origem[NUM_CHAR];
     char cidade_destino[NUM_CHAR];
     int escalas;
 };
 
+//PROTÓTIPOS
+int inclusao_voo(struct Viagem V[]);
+void alteracao_info_voo(struct Viagem V[], int quant_voo);
+// void exclusao_voo();
+// void quant_voo_origem();
+// void menor_quant_escala_voo();
+
 int main(){
-    struct Voo viagem [NUM_VIAGEM];
-    int resposta = 0;
+    struct Viagem viagem[NUM_VIAGEM];
+    int resposta = 0, n_viagem = 0;
     // char resposta = 0;
 
-    // do{
+    do{
+        // system("cls");
+        printf("VERMELHO LINHAS AÉREAS\n");
+        printf("Por favor, selecionar as opções abaixo:\n");
+        printf("1.Incluir voos.\n");
+        printf("2.Alterar as informações do voo.\n");
+        printf("3.Apagar voo.\n");
+        printf("4.Quantidade de voos da cidade de origem.\n");
+        printf("5.Quantidade de voos com menor número de escala da cidade de origem e destino.\n");
+        printf("0.Sair\n");
+        scanf("%d",&resposta);
 
-    // }while();
-    printf("VERMELHO LINHAS AÉREAS\n");
-    printf("Por favor, selecionar as opções abaixo:\n");
-    scanf("%d",&resposta);
+        switch (resposta) {
+            case 1:
+                // printf("\nPassou");
+                n_viagem=inclusao_voo(viagem);
+                // n_viagem = inclusao_voo(viagem);
+                break;
+            case 2:
+                alteracao_info_voo(viagem, n_viagem);
+                break;
+            // case 3:
+            //     exclusao_voo();
+            //     break;
+            // case 4:
+            //     quant_voo_origem();
+            //     break;
+            // case 5:
+            //     menor_quant_escala_voo();
+            //     break;
+            case 0:
+                return 0;
+            default:
+                printf("Opção inválida, tente novamente.\n");
+                break;
+        }
 
-    switch (resposta) {
-        case 1:
-            printf("Incluir voos.\n");
-            inclusao_voo(viagem);
-            break;
-        case 2:
-            printf("Alterar as informações do voo.\n");
-            alteracao_info_voo();
-            break;
-        case 3:
-            printf("Apagar voo.\n");
-            exlusao_voo();
-            break;
-        case 4:
-            printf("Quantidade de voos da cidade de origem.\n");
-            quant_voo_origem();
-            break;
-        case 5:
-            printf("Quantidade de voos com menor número de escala da cidade de origem e destino.\n");
-            menor_quant_escala_voo();
-            break;
-        case 6:
-            printf("Sair.\n");
-            return 0;
-            break;
-        default:
-            printf("Opção inválida, tente novamente.\n");
-            break;
-    }
+    }while(resposta != 0);
 
 }
 
-void inclusao_voo(Voo viagem[]){
-    int quant_voo = 0, contador = 1;
+int inclusao_voo(struct Viagem V[]){
+    int quant_voo = 1, contador =1, i = 0;
+    
     printf("\nDeseja incluir quantos voos?");
-    scanf("%d",&quant_voo);
-    //Verificaçao da quantidade de voo, que nao pode exceder de 50
-    //do{
-
-    //}while(quant_voo<50);
-
+    while(scanf("%d",&quant_voo) != ((quant_voo>0) && (quant_voo<50))){
+        printf("Entrada inválida. Digite um valor entre 1 e 49: ");
+    };
+    
     do{
         printf("\nVIAGEM %d:",contador);
         printf("\nDigite o código da viagem: ");
-        scanf("%d",%Voo[quant_voo].codigo);
+        while(scanf("%d",&V[i].codigo) != (V[i].codigo >=1000 && V[i].codigo <= 9999)){
+            printf("O número não possui 4 dígitos. Digite novamente.\n");
+        }
         printf("\nDigite a cidade de origem: ");
-        scanf("%d",%Voo[quant_voo].cidade_origem);
+        getchar();
+        gets(V[i].cidade_origem);
+        printf("\nDigite a cidade de destino: ");
+        gets(V[i].cidade_destino);
+        printf("\nDigite a quantidade de paradas: ");
+        scanf("%d", &V[i].escalas);
+        printf("\n");
 
-    }while(quant_voo !=0);
+        quant_voo--;
+        contador++;
+        i++;
+    }while(quant_voo > 0);
 
+    return contador;
+
+}
+
+void alteracao_info_voo(struct Viagem V[], int quant_voo){
+    int resposta = 0;
+    printf("VIAGENS CADASTRADAS:\n");
+    for(int i = 1; i<quant_voo; i++){
+        printf("%d Viagem %d;\n",i);
+    }
+    printf("\nQual das viagens deseja alterar: \n");
+    while(scanf("%d",&resposta) != (resposta>0 && resposta<=quant_voo));
+    
+    // for(int i = 0; i<quant_voo-1; i++){
+        
+    //     printf("\nVIAGEM %d:",i+1);
+    //     printf("\nDigite o código da viagem: %d", V[i].codigo);
+    //     printf("\nDigite a cidade de origem: %s", V[i].cidade_origem);
+    //     printf("\nDigite a cidade de destino: %s", V[i].cidade_destino);
+    //     printf("\nDigite a quantidade de paradas: %d\n", V[i].escalas);
+        
+
+    // }
 
 }
